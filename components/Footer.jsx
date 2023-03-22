fad485da19504a23ffdd3b8693ac4c70d84af644
ThapeloMasasa <tmasasa@nd.edu>
2023-03-22 18:11:28 -0400

    Added products

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai'
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+            <p>
+                2022 MSN Headphones All rights reserved
+            </p>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import product from '@/shop/schemas/product'
-import React from 'react'
+import product from '@/shop/schemas/product';
+import React from 'react';
+import { Product,FooterBanner, HeroBanner } from '@/components';
+import {client} from '../lib/client';
 
-const Home= () => {
+const Home= ({products, bannerData}) => {
   return (
     <>
-    HeroBanner 
-
+    <HeroBanner bannerData ={bannerData.length && bannerData[0]}/>
+    
     <div className='products-heading'>
       <h2>
           Best Selling Products
@@ -14,17 +16,32 @@ const Home= () => {
           Speakers of many variations
       </p>
       <div className='products-container'>
+       
         {
-          ['Product 1', 'Product 2'].map(
-            (product) => product
+         
+        products?.map(
+            (product) => <Product key={product._id} product={product}/>
           )
         }
       </div>
     </div>
 
-    Footer
+    <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   )
 }
 
+export const getServerSideProps = async () =>{
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props:{
+      products, bannerData
+    }
+  }
+}
 export default Home
