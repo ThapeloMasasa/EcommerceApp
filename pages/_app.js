94b21e4ed4ac28d1927ec05a00c213cdb41593ec
ThapeloMasasa <tmasasa@nd.edu>
2023-04-01 18:35:06 -0400

    fixed name

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
-import { StateContext } from '../context/StateContext';
+import { StateContext } from '../context/Statecontext';
 
 function MyApp({ Component, pageProps }) {
   return (
