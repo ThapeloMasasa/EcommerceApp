da64937b6f6dafdf7c62884285ade8ca8b10694b
ThapeloMasasa <tmasasa@nd.edu>
2023-04-01 18:37:47 -0400

    fixed name

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,7 +3,7 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
-import { useStateContext } from '../../context/StateContext';
+import { useStateContext } from '../../context/Statecontext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
